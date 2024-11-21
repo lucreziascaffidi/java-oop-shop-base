33206687969a824d52cad9d47ec6990bff80315a
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-11-21 19:10:17 +0100

    feat: add methods

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 
     // Variabili di istanza
@@ -8,4 +10,33 @@ public class Prodotto {
     String descrizione;
     float prezzo;
     float iva;
+
+    // Metodi
+
+    // Costruttore
+    public Prodotto(String nome, String descrizione, float prezzo, float iva) {
+        Random random = new Random();
+        this.codice = random.nextInt(777777);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public void generaCodice() {
+        return this.codice;
+    }
+
+    public float prezzoBase() {
+        return this.prezzo;
+    }
+
+    public float prezzoConIva() {
+        return this.prezzo + this.prezzo * this.iva;
+    }
+
+    public String nomeProdotto() {
+        return this.codice + "-" + nome;
+    }
+
 }
