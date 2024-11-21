f0bca0e6f5437c1fc4bfb130a6d1bf1b1f18566d
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-11-21 18:50:58 +0100

    feat: add class with variables

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,11 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+    // Variabili di istanza
+    int codice;
+    String nome;
+    String descrizione;
+    float prezzo;
+    float iva;
+}
